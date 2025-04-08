a02fafbe61cbf385f034898dd4c0cf3b9aaf97c1
maoyixie <maoyi001@e.ntu.edu.sg>
2025-04-08 20:06:11 +0800

    modify for testing normal and abnormal

---
+++ align_test_B.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+# Setting the Path
+anno_file = "./anno/B_anno.csv"
+test_b_file = "./data/B.tsv"
+output_file = "./data/B_aligned.tsv"
+
+# Read the probe sequence for training
+anno_df = pd.read_csv(anno_file, sep=None, engine='python')
+if '#id' in anno_df.columns:
+    probe_ids = anno_df['#id'].tolist()
+elif 'Composite Element REF' in anno_df.columns:
+    probe_ids = anno_df['Composite Element REF'].tolist()
+elif 'id' in anno_df.columns:
+    probe_ids = anno_df['id'].tolist()
+else:
+    raise ValueError("Cannot find probe id column in B_anno.csv")
+
+# Read test's B.tsv
+test_b = pd.read_csv(test_b_file, sep='\t', index_col=0)
+
+# Re-index to ensure that the row order is consistent and missing items are filled with 0
+aligned_b = test_b.reindex(probe_ids).fillna(0)
+
+# Writing out files
+aligned_b.to_csv(output_file, sep='\t')
+print(f"✅ Finished: Output aligned test B.tsv to {output_file}")
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
+### test.py
 """
-The testing part for OmiTrans
+The testing part for OmiTrans (generate fake_A only)
 """
 import time
 from util import util
@@ -8,7 +9,6 @@ from datasets import create_single_dataloader
 from models import create_model
 from util.visualizer import Visualizer
 
-
 if __name__ == '__main__':
     # Get testing parameter
     param = TestParams().parse()
@@ -16,44 +16,35 @@ if __name__ == '__main__':
         util.setup_seed(param.seed)
 
     # Dataset related
-    dataloader, param.sample_list = create_single_dataloader(param, shuffle=False)  # No shuffle for testing
+    dataloader, param.sample_list = create_single_dataloader(param, shuffle=False)
     print('The size of testing set is {}'.format(len(dataloader)))
-    # Get sample list of the dataset
     sample_list = dataloader.get_sample_list()
-    # Get feature list of the dataset
-    feature_list_A = dataloader.get_feature_list_A()
-    # Get the dimension of two omics types
+    feature_list_A = ["fake_feature_{}".format(i) for i in range(dataloader.get_A_dim())]  # Dummy feature names
     param.A_dim = dataloader.get_A_dim()
     param.B_dim = dataloader.get_B_dim()
     print('The dimension of omics type A is %d' % param.A_dim)
-    print('The dimension of omics type B is %d' % param.B_dim)
+    print('The dimension of omics type B is ' % param.B_dim)
     if param.zo_norm:
         param.target_min = dataloader.get_values_min()
         param.target_max = dataloader.get_values_max()
 
-    # Model related
-    model = create_model(param)                 # Create a model given param.model and other parameters
+    model = create_model(param)
     model.set_eval()
-    visualizer = Visualizer(param)              # Create a visualizer to print results
+    visualizer = Visualizer(param)
 
-    # TESTING
-    model.setup(param)  # load saved networks for testing
-    metrics_acc = model.init_metrics_dict()  # Initialize the metrics dictionary
-    if param.save_fake:  # Initialize the fake array during the last epoch
+    model.setup(param)
+    if param.save_fake:
         fake_dict = model.init_fake_dict()
-    test_start_time = time.time()  # Start time of testing
+
+    test_start_time = time.time()
 
     for i, data in enumerate(dataloader):
-        dataset_size = len(dataloader)
-        model.set_input(data)  # Unpack input data from the output dictionary of the dataloader
-        model.test()  # Run forward to get the fake omics data
-        model.update_metrics_dict(metrics_acc)  # Update the metrics dictionary
-        if param.save_fake:  # Update the fake array during the last epoch
+        model.set_input(data)
+        model.test()
+        if param.save_fake:
             fake_dict = model.update_fake_dict(fake_dict)
-        visualizer.print_test_log(param.epoch_to_load, i, metrics_acc, param.batch_size, dataset_size)
-    test_time = time.time() - test_start_time
-    # Save average metrics of this epoch to the disk
-    visualizer.print_test_summary(param.epoch_to_load, metrics_acc, test_time)
 
-    if param.save_fake:  # Save the fake omics data
+    if param.save_fake:
         visualizer.save_fake_omics(fake_dict, sample_list, feature_list_A)
+
+    print("Done generating fake A")
